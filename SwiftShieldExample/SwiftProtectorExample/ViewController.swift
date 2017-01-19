acf98af7562bbc76bb09a14b774a37b354686a96
bruno-rocha-movile <bruno.rocha@movile.com>
2017-01-19 10:33:51 -0200

    Class update

--- SwiftShieldExample/SwiftProtectorExample/ViewController.swift
+++ SwiftShieldExample/SwiftProtectorExample/ViewController.swift
@@ -1,7 +1,27 @@
-class ViewController {
-    
-}
+//
+//  ViewController.swift
+//  gpstest
+//
+//  Created by Bruno Rocha on 8/5/16.
+//  Copyright © 2016 Movile. All rights reserved.
+//
 
-struct SuperStruct {
+import UIKit
+import Foundation
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
     
 }
