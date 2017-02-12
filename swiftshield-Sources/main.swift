17d661c07640f5e2d4699d5675bdaaacbd4c3b79
bruno-rocha-movile <bruno.rocha@movile.com>
2017-02-12 12:18:28 -0200

    Binary

--- swiftshield-Sources/main.swift
+++ swiftshield-Sources/main.swift
@@ -1,9 +1,9 @@
 import Foundation
 
-let basePath = UserDefaults.standard.string(forKey: "projectroot") ?? "/Users/bruno.rocha/Desktop/vivo-meditacao-ios"
-let mainScheme = UserDefaults.standard.string(forKey: "scheme") ?? "aaa"
+let basePath = UserDefaults.standard.string(forKey: "projectroot") ?? ""
+let mainScheme = UserDefaults.standard.string(forKey: "scheme") ?? ""
 
-let projectToBuild = UserDefaults.standard.string(forKey: "projectfile") ?? "/Users/bruno.rocha/Desktop/vivo-meditacao-ios/VivoMeditacao.xcworkspace"
+let projectToBuild = UserDefaults.standard.string(forKey: "projectfile") ?? ""
 
 guard basePath.isEmpty == false, mainScheme.isEmpty == false, projectToBuild.isEmpty == false else {
     Logger.log("Bad arguments.\n\nRequired parameters:\n\n-projectroot PATH (Path to your project root, like /app/MyApp \n\n-projectfile PATH (Path to your project file, like /app/MyApp/MyApp.xcodeproj or /app/MyApp/MyApp.xcworkspace)\n\n-scheme 'SCHEMENAME' (Main scheme to build)\n\nOptional parameters:\n\n-ignoreschemes 'NAME1,NAME2,NAME3' (If you have multiple schemes that point to the same target, like MyApp-CI or MyApp-Debug, mark them as ignored to prevent errors)\n\n-v (Verbose mode)")
