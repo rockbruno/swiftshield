83345d5c623905f6a4f646e872d1a429b3c47760
Bruno Rocha <brunorochaesilva@gmail.com>
2019-04-13 14:18:08 -0300

    Updating run script

--- SwiftShieldExample/_CDToMeAndRunMeToObfuscate.sh
+++ SwiftShieldExample/_CDToMeAndRunMeToObfuscate.sh
@@ -1,2 +1,4 @@
 pod install
-../bin/swiftshield -project-root ./ -automatic -automatic-project-file ./SwiftShieldExample.xcworkspace -automatic-project-scheme SwiftShieldExample
+echo "Running sudo chmod to make the pods folder writable"
+sudo chmod -R 774 ./Pods
+../bin/swiftshield -project-root ./ -automatic -automatic-project-file ./SwiftShieldExample.xcworkspace -automatic-project-scheme SwiftShieldExample -verbose
